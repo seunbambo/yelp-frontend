19c73cbd5bbc82b6dcbadac88704e012bbbf25ad
Oluwaseun Adebambo <emma.bambo@yahoo.com>
2020-03-20 14:40:52 +0100

    adapt layout

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -7,7 +7,7 @@ import { SearchSuggestions } from "./SearchSuggestions/SearchSuggestions";
 
 export function LandingPage() {
   return (
-    <div>
+    <div className={styles.landing}>
       <div className={styles["search-area"]}>
         <TopNav />
         <img src={logo} className={styles.logo} alt="logo" />
