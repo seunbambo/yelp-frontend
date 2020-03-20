572d2932cb1d2fd9283c94408df331b774673ba7
Oluwaseun Adebambo <emma.bambo@yahoo.com>
2020-03-20 10:55:14 +0100

    logo

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { TopNav } from "./TopNav/TopNav";
+import logo from "../assets/logo.png";
+import styles from "./LandingPage.module.css";
 
 export function LandingPage() {
-  return <TopNav />;
+  return (
+    <>
+      <TopNav />
+      <img src={logo} className={styles.logo} alt="logo" />
+    </>
+  );
 }
