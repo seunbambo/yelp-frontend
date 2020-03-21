bf0a88e62ab3428fe330219d16367347f8fc26c2
Oluwaseun Adebambo <emma.bambo@yahoo.com>
2020-03-21 12:56:55 +0100

    SearchResultsSummary

---
+++ src/Search/SearchResultsSummary/SearchResultsSummary.js
@@ -0,0 +1,41 @@
+import React from "react";
+import styles from "./SearchResultsSummary.module.css";
+
+export function SearchResultsSummary() {
+  return (
+    <div className={styles.container}>
+      <div className={styles["search-summary"]}>
+        <h1 className="subtitle">
+          <strong>burgers</strong> berlin
+        </h1>
+        <p>Showing 1-20 out of 543 results</p>
+      </div>
+      <div className="styles.filters">
+        <button className="button">
+          <span className="icon">
+            <i className="fas fa-sliders-h"></i>
+          </span>
+          <span>All Filters</span>
+        </button>
+        <div className="buttons has-addons">
+          <button className="button">$</button>
+          <button className="button">$$</button>
+          <button className="button">$$$</button>
+          <button className="button">$$$$</button>
+        </div>
+        <button className="button">
+          <span className="icon">
+            <i className="fas fa-clock"></i>
+          </span>
+          <span>Open Now</span>
+        </button>
+        <button className="button">
+          <span className="icon">
+            <i className="fas fa-dollar-sign"></i>
+          </span>
+          <span>Cashback</span>
+        </button>
+      </div>
+    </div>
+  );
+}
