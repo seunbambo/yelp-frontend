285992054978288f8b719073255c0aba353fbb50
Oluwaseun Adebambo <emma.bambo@yahoo.com>
2020-03-21 11:50:38 +0100

    sub nav

--- src/NavBar/SubNav/SubNav.js
+++ src/NavBar/SubNav/SubNav.js
@@ -1,13 +1,34 @@
 import React from "react";
 import { SubNavItem } from "./SubNavItem/SubNavItem";
+import styles from "./SubNav.module.css";
 
 export function SubNav() {
   return (
-    <div>
-      <SubNavItem label="Restaurant" icon="fa-utensils" />
-      <SubNavItem label="Home Services" icon="fa-utensils" />
-      <SubNavItem label="Auto Services" icon="fa-utensils" />
-      <SubNavItem label="More" icon="fa-utensils" />
+    <div className={styles["container"]}>
+      <div className={styles["sub-nav"]}>
+        <div>
+          <SubNavItem label="Restaurant" icon="fa-utensils" />
+          <SubNavItem label="Home Services" icon="fa-home" />
+          <SubNavItem label="Auto Services" icon="fa-car-side" />
+          <SubNavItem label="More" icon="fa-info-circle" showRightBorder />
+        </div>
+        <div>
+          <button
+            className={`button ${styles["subnav-button"]} ${styles["omit-right-border"]}`}
+          >
+            <span className="icon">
+              <i className="fas fa-pen"></i>
+            </span>
+            <span>Write a Review</span>
+          </button>
+          <button className={`button ${styles["subnav-button"]}`}>
+            <span className="icon">
+              <i className="fas fa-hotel"></i>
+            </span>
+            <span>For Business</span>
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/NavBar/SubNav/SubNavItem/SubNavItem.js
+++ src/NavBar/SubNav/SubNavItem/SubNavItem.js
@@ -1,24 +1,25 @@
 import React from "react";
+import styles from "./SubNavItem.module.css";
 
 export function SubNavItem(props) {
+  const borderClass = props.showRightBorder ? styles["right-border"] : "";
+
   return (
     <div className="dropdown is-hoverable">
       <div className="dropdown-trigger">
-        <button
-          className="button"
+        <div
+          className={`${styles["sub-nav-item"]} ${borderClass}`}
           aria-haspopup="true"
           aria-controls="dropdown-menu4"
         >
           <span className="icon is-small">
             <i className={`fas ${props.icon}`} />
           </span>
-          <span>
-            {props.label}
-          </span>
+          <span>{props.label}</span>
           <span className="icon is-small">
             <i className="fas fa-angle-down" aria-hidden="true" />
           </span>
-        </button>
+        </div>
       </div>
       <div className="dropdown-menu" id="dropdown-menu4" role="menu">
         <div className="dropdown-content">
